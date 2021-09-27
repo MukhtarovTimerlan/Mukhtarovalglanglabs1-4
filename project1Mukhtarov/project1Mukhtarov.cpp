77fd8f6c16e43bcd53b83d4fa3f5a2a00192ff5c
Timerlan <tima-m02@mail.ru>
2021-09-27 13:32:27 +0300

    Update project1Mukhtarov.cpp

--- project1Mukhtarov/project1Mukhtarov.cpp
+++ project1Mukhtarov/project1Mukhtarov.cpp
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <string>
 
+using std::cin;
+using std::cout;
+using std::endl;
+
 struct Pipe
 {
     int id;
@@ -21,10 +25,10 @@ Pipe input_pipe()
 {
     Pipe pipe1;
     pipe1.id = 0;
-    std::cout << "Input length of pipe - ";
-    std::cin >> pipe1.length;
-    std::cout << "Input diametr of pipe - ";
-    std::cin >> pipe1.diametr;
+    cout << "Input length of pipe - ";
+    cin >> pipe1.length;
+    cout << "Input diametr of pipe - ";
+    cin >> pipe1.diametr;
     return pipe1;
 }
 
@@ -32,41 +36,65 @@ CS input_CS()
 {
     CS CS1;
     CS1.id = 0;
-    std::cout << "Input name of CS - ";
-    std::cin >> CS1.name_CS;
-    std::cout << "Input count of CS - ";
-    std::cin >> CS1.count_of_CS;
-    std::cout << "Input count of CS in work - ";
-    std::cin >> CS1.count_of_CS_in_work;
-    std::cout << "Input effective - ";
-    std::cin >> CS1.effective;
+    cout << "Input name of CS - ";
+    cin >> CS1.name_CS;
+    cout << "Input count of CS - ";
+    cin >> CS1.count_of_CS;
+    cout << "Input count of CS in work - ";
+    cin >> CS1.count_of_CS_in_work;
+    cout << "Input effective - ";
+    cin >> CS1.effective;
     return CS1;
 }
 
 void output(Pipe& pipe1)
 {
-    std::cout << "Length = " << pipe1.length<< '\n';
-    std::cout << "id of pipe = " << pipe1.id << '\n';
-    std::cout << "Diametr = " << pipe1.diametr << '\n';
+    cout << "Length = " << pipe1.length<< '\n';
+    cout << "id of pipe = " << pipe1.id << '\n';
+    cout << "Diametr = " << pipe1.diametr << '\n';
 }
 
 void output(CS& CS1)
 {
-    std::cout << "id of CS = " << CS1.id << '\n';
-    std::cout << "Name of CS = " << CS1.name_CS << '\n';
-    std::cout << "Count of CS = " << CS1.count_of_CS << '\n';
-    std::cout << "Count of CS in work = " << CS1.count_of_CS_in_work << '\n';
-    std::cout << "Effective of CS = " << CS1.effective << '\n';
+    cout << "id of CS = " << CS1.id << '\n';
+    cout << "Name of CS = " << CS1.name_CS << '\n';
+    cout << "Count of CS = " << CS1.count_of_CS << '\n';
+    cout << "Count of CS in work = " << CS1.count_of_CS_in_work << '\n';
+    cout << "Effective of CS = " << CS1.effective << '\n';
+}
+
+void print_menu() {
+    system("cls"); // очищаем экран
+    cout << "What do you want to do?" << endl;
+    cout << "1. Add pipe" << endl;
+    cout << "2. Add CS" << endl;
+    cout << "3. Show all objects" << endl;
+    cout << "4. Edit pipe" << endl;
+    cout << "5. Edit CS" << endl;
+    cout << "6. Save changes" << endl;
+    cout << "7. Load to file" << endl;
+    cout << "0. Exit" << endl;
+    cout << ">";
+}
+
+Pipe change_pipe(pipe1)
+{
+        input_pipe();
+        cout << "Pipe after change" << '\n';
+        output(pipe1);
+
 }
 
 int main()
 {
+    print_menu();
     Pipe pipe1;
     pipe1 = input_pipe();
     output(pipe1);
     CS CS1;
     CS1 = input_CS();
     output(CS1);
+    pipe1 change_pipe();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
